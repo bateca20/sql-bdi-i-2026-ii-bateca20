8ef5004e0292b61e2d2b5445dacb54dbcd20de30
bateca20 <juan.carvajalbat@unipamplona.edu.co>
2026-05-06 23:37:27 -0500

    feat(sql): add university sql scripts and database pipelines

---
+++ content/university/data/Sql/03-BUILDINGS.sql
@@ -0,0 +1,6 @@
+INSERT INTO university.buildings (building_id,name) VALUES
+(1, 'Bloque A - Ingenierías'),
+(2, 'Bloque B - Ciencias Básicas'),
+(3, 'Bloque C - Humanidades'),
+(4, 'Bloque D - Administración'),
+(5, 'Centro de Cómputo');
