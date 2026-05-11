4d1300c37ded0218e850db15838b4cbbb826cc3b
bateca20 <juan.carvajalbat@unipamplona.edu.co>
2026-05-11 18:49:13 -0500

    feat(sql): update database queries

--- content/university/scripts/ddl/Queries.sql
+++ content/university/scripts/ddl/Queries.sql
@@ -5,12 +5,12 @@ SELECT
     university.users.last_name,
     university.programs.name AS program,
     university.programs_students.current_semester
-FROM university.programs_students
-JOIN university.programs
-    ON university.programs_students.program_id = university.programs.program_id
-JOIN university.users
-    ON university.programs_students.user_id = university.users.user_id
-WHERE university.programs.name = 'Ingeniería de Sistemas'
+FROM university.programs_students,
+     university.programs,
+     university.users
+WHERE university.programs_students.program_id = university.programs.program_id
+AND university.programs_students.user_id = university.users.user_id
+AND university.programs.name = 'Ingeniería de Sistemas'
 AND university.programs_students.current_semester = 5;
 
 
@@ -19,10 +19,10 @@ AND university.programs_students.current_semester = 5;
 SELECT 
     university.users.first_name || ' ' || university.users.last_name AS full_name,
     university.users.email
-FROM university.users
-JOIN university.roles
-    ON university.users.role_id = university.roles.role_id
-WHERE university.roles.name = 'Professor';
+FROM university.users,
+     university.roles
+WHERE university.users.role_id = university.roles.role_id
+AND university.roles.name = 'Professor';
 
 
 
@@ -32,10 +32,10 @@ WHERE university.roles.name = 'Professor';
 SELECT 
     university.courses.name AS course,
     COUNT(DISTINCT university.programs.faculty_id) AS faculties_count
-FROM university.programs_courses
-JOIN university.programs
-    ON university.programs_courses.program_id = university.programs.program_id
-JOIN university.courses
-    ON university.programs_courses.course_id = university.courses.course_id
+FROM university.programs_courses,
+     university.programs,
+     university.courses
+WHERE university.programs_courses.program_id = university.programs.program_id
+AND university.programs_courses.course_id = university.courses.course_id
 GROUP BY university.courses.name
 HAVING COUNT(DISTINCT university.programs.faculty_id) > 1;
